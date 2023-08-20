fbb8177c0326b96c9c5018c99d6de76b728c7850
sosadwaden <wadenjap@gmail.com>
2023-08-20 18:13:53 +0300

    init

---
+++ src/main/java/com/sosadwaden/pasteboxtesttask/PasteboxTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sosadwaden.pasteboxtesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteboxTestTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasteboxTestTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sosadwaden/pasteboxtesttask/PasteboxTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosadwaden.pasteboxtesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteboxTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
